db88d5f6f64660d50459f2a447ad10af0ce71015
Tulai Dmitry <dtylai@icloud.com>
2023-10-23 22:32:23 +0300

    feat: Initial commit

---
+++ metaPrice/metaPrice/metaPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  metaPriceApp.swift
+//  metaPrice
+//
+//  Created by Tulai Dima on 23.10.23.
+//
+
+import SwiftUI
+
+@main
+struct metaPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
